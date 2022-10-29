e3f270dd77247ad5eacb9999a013def0a90cfcc1
0xklapouchy <0xklapouchy@gmail.com>
2022-10-29 11:46:08 +0200

    Update Kyc3 module tests

--- contracts/test/Kyc3.test.ts
+++ contracts/test/Kyc3.test.ts
@@ -1,54 +1,103 @@
 import { waffle } from "hardhat";
 import { expect } from "chai";
 
-import Kyc3Artifact from "../artifacts/contracts/core/Kyc3.sol/Kyc3.json";
 import Kyc3MockArtifact from "../artifacts/contracts/mocks/Kyc3Mock.sol/Kyc3Mock.json";
 
-import { Kyc3, Kyc3Mock } from "../typechain";
-import { Wallet, utils, BigNumber } from "ethers";
+import { Kyc3Mock } from "../typechain";
+import { Wallet, BigNumber } from "ethers";
 
-import { latest } from "./utilities";
+import { latest, injectKycData, duration } from "./utilities";
 
 const { provider, deployContract } = waffle;
 
+// Error codes
+const Error_Age: string = "Error_Age()";
+const Error_Country: string = "Error_Country()";
+const Error_Timestamp: string = "Error_Timestamp()";
+
 describe("Kyc3 Module Tests", () => {
   const [deployer, user, signer] = provider.getWallets() as Wallet[];
-  let kyc3: Kyc3;
   let kyc3mock: Kyc3Mock;
 
   let now: BigNumber;
-
-  beforeEach(async () => {
-    kyc3 = (await deployContract(deployer, Kyc3Artifact, [])) as Kyc3;
-    now = await latest();
-  });
 
   describe("getKycData function", () => {
     beforeEach(async () => {
       kyc3mock = (await deployContract(deployer, Kyc3MockArtifact, [])) as Kyc3Mock;
       await kyc3mock.addSigner(signer.address);
+      now = await latest();
     });
 
-    it("should work", async () => {
-      const data = utils.defaultAbiCoder.encode(
-        ["bytes32", "uint8", "uint16", "uint32"],
-        ["0x12ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcd", 22, 34, 1667096757]
+    it("should work correctly", async () => {
+      const id = "0x12ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcd";
+      const age = 22;
+      const country = 34;
+      const timestamp = now.add(duration.minutes(5)).toNumber();
+
+      const unsignedTx = await injectKycData(
+        await kyc3mock.connect(user).populateTransaction.example(100, "0x0000000000000000000000000000000000000009"),
+        id,
+        age,
+        country,
+        timestamp,
+        signer
       );
-      const size = utils.defaultAbiCoder.encode(["uint8"], [4]).slice(64);
-      const deadline = utils.defaultAbiCoder.encode(["uint32"], [now]).slice(58);
 
-      const payload = data + size + deadline;
-      const payloadHash = utils.keccak256(payload);
+      await expect(user.sendTransaction(unsignedTx)).to.not.be.reverted;
+    });
 
-      const signature = await signer.signMessage(utils.arrayify(payloadHash));
+    it("should revert when age < 22", async () => {
+      const id = "0x12ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcd";
+      const age = 12;
+      const country = 34;
+      const timestamp = now.add(duration.minutes(5)).toNumber();
 
-      const unsignedTx = await kyc3mock
-        .connect(user)
-        .populateTransaction.example(100, "0x0000000000000000000000000000000000000009");
+      const unsignedTx = await injectKycData(
+        await kyc3mock.connect(user).populateTransaction.example(100, "0x0000000000000000000000000000000000000009"),
+        id,
+        age,
+        country,
+        timestamp,
+        signer
+      );
 
-      unsignedTx.data = unsignedTx.data + payload.slice(2) + signature.slice(2);
+      await expect(user.sendTransaction(unsignedTx)).to.be.revertedWith(Error_Age);
+    });
 
-      await user.sendTransaction(unsignedTx);
+    it("should revert when country == 33", async () => {
+      const id = "0x12ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcd";
+      const age = 23;
+      const country = 33;
+      const timestamp = now.add(duration.minutes(5)).toNumber();
+
+      const unsignedTx = await injectKycData(
+        await kyc3mock.connect(user).populateTransaction.example(100, "0x0000000000000000000000000000000000000009"),
+        id,
+        age,
+        country,
+        timestamp,
+        signer
+      );
+
+      await expect(user.sendTransaction(unsignedTx)).to.be.revertedWith(Error_Country);
+    });
+
+    it("should revert when timestamp to old", async () => {
+      const id = "0x12ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcd";
+      const age = 23;
+      const country = 4;
+      const timestamp = now.toNumber();
+
+      const unsignedTx = await injectKycData(
+        await kyc3mock.connect(user).populateTransaction.example(100, "0x0000000000000000000000000000000000000009"),
+        id,
+        age,
+        country,
+        timestamp,
+        signer
+      );
+
+      await expect(user.sendTransaction(unsignedTx)).to.be.revertedWith(Error_Timestamp);
     });
   });
 });
