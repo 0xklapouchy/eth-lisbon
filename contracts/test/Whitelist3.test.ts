df553a981220336d4d933e2d0571c15a43ccf804
0xklapouchy <0xklapouchy@gmail.com>
2022-10-29 04:52:01 +0200

    Remove redundant pack functions tests

--- contracts/test/Whitelist3.test.ts
+++ contracts/test/Whitelist3.test.ts
@@ -16,31 +16,5 @@ describe("Whitelist3 Module Tests", () => {
     whitelist3 = (await deployContract(deployer, Whitelist3Artifact, [])) as Whitelist3;
   });
 
-  describe("packWhitelist function", () => {
-    it("should correctly pack whitelist data", async () => {
-      expect(
-        await whitelist3.packWhitelist("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcd", 1666992943)
-      ).to.be.equal(
-        "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcd00000000000000000000000000000000000000000000000000000000635c4b2f"
-      );
-    });
-
-    it("should correctly pack whitelist data with max values", async () => {
-      expect(
-        await whitelist3.packWhitelist("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 4294967295)
-      ).to.be.equal(
-        "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff00000000000000000000000000000000000000000000000000000000ffffffff"
-      );
-    });
-
-    it("should revert when id != bytes32", async () => {
-      await expect(whitelist3.packWhitelist("0xff", 1666992943)).to.be.reverted;
-    });
-
-    it("should revert when timestamp > uint32", async () => {
-      await expect(
-        whitelist3.packWhitelist("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffabcd", 4294967296)
-      ).to.be.reverted;
-    });
-  });
+  describe("getWhitelistedData function", () => {});
 });
